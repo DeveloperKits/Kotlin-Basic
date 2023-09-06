c8ca6e90dc5b7c7c9e2f85a76ddf0b5d95624f77
Akash Das <akashdas.dev@gmail.com>
2023-09-06 20:02:31 +0600

    survive orientation screen change

--- app/src/main/java/com/example/kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.kotlinbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlinbasic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var resultSum = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState != null){
+            resultSum = savedInstanceState.getInt("result")
+            binding.result.text = resultSum.toString()
+        }
+
+        binding.calculate.setOnClickListener{
+            resultSum =
+                binding
+                .editTextNumber
+                .text
+                .toString()
+                .toInt() +
+                    binding
+                    .editTextNumber2
+                    .text
+                    .toString()
+                    .toInt()
+
+            binding.result.text = resultSum.toString()
+        }
+
+    }
+
+    // when orientation screen change then show same calculate result data
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt("result", resultSum)
+        super.onSaveInstanceState(outState)
     }
 }
